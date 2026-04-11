4ec63fe66a946cfc9c69e28ab6710485c1f21b41
Yasitha Peiris <yasithapeiris68@gmail.com>
2026-04-11 20:08:44 +0530

    Project initialization.

---
+++ src/main/java/com/smartcampus/HelloWorldResource.java
@@ -0,0 +1,25 @@
+package com.smartcampus;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+/**
+ * Root resource (exposed at "test" path)
+ */
+@Path("test")
+public class HelloWorldResource {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Hello World! The Grizzly server and Jersey dependencies are working.";
+    }
+}
